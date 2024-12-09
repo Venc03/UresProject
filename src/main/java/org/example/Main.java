f40f77d891a896e2204ef0a984cc40dfcfade2d4
vegi.daniel <vegi.dani1@gmail.com>
2024-12-09 13:51:49 +0100

    F1 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        Tabla t1 = new Tabla('-');
     }
 }
--- src/main/java/org/example/Tabla.java
+++ src/main/java/org/example/Tabla.java
@@ -5,13 +5,15 @@ public class Tabla {
     private char UresCella;
 
     public Tabla(char uresCella) {
-        UresCella = uresCella;
+        this.UresCella = uresCella;
         T = new char[8][8];
         for (int i = 0; i < T.length; i++) {
             for (int j = 0; j < T.length; j++) {
                 T[i][j] = this.UresCella;
             }
         }
+
+        Megjelenit();
     }
 
     public void UresOszlopokSzama(){
@@ -31,7 +33,18 @@ public class Tabla {
     }
 
     public void Megjelenit(){
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T.length; j++) {
+                if(j == T.length - 1){
+                    System.out.print(T[i][j]);
+                    System.out.print("\n");
+                }
+                else {
+                    System.out.print(T[i][j]);
+                }
 
+            }
+        }
     }
 
     public void Tabla(){
