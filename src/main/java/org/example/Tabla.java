85619b302bad9e2b4e71fef1331e2b19ed2de664
vegi.daniel <vegi.dani1@gmail.com>
2024-12-09 13:30:16 +0100

    F1 1

---
+++ src/main/java/org/example/Tabla.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class Tabla {
+    private int[][] T;
+    private char UresCella;
+
+    public void UresOszlopokSzama(){
+
+    }
+
+    public void UresSorokSzama(){
+
+    }
+
+    public void Elhelyezes(){
+
+    }
+
+    public void Fajlbairas(){
+
+    }
+
+    public void Megjelenit(){
+
+    }
+
+    public void Tabla(){
+
+    }
+
+    public void UresOszlop(){
+
+    }
+
+    public void UresSor(){
+
+    }
+}
