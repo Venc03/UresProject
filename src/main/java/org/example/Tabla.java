74c46f778ff85f445c1052e489ddb43847d3566a
vegi.daniel <vegi.dani1@gmail.com>
2024-12-09 13:40:31 +0100

    F1 3

--- src/main/java/org/example/Tabla.java
+++ src/main/java/org/example/Tabla.java
@@ -1,9 +1,18 @@
 package org.example;
 
 public class Tabla {
-    private int[][] T;
+    private char[][] T;
     private char UresCella;
 
+    public Tabla(char uresCella) {
+        UresCella = uresCella;
+        T = new char[8][8];
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T.length; j++) {
+                T[i][j] = this.UresCella;
+            }
+        }
+    }
 
     public void UresOszlopokSzama(){
 
